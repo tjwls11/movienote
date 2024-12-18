3c3c3ea31306557d33bfade3ba23b10c66393dc9
tjwls11 <09sj03@naver.com>
2024-12-19 00:11:16 +0900

    css

--- next.config.js
+++ next.config.js
@@ -16,6 +16,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
